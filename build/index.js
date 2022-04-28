ed8c6fb74c38a466b9002f4b70dffb1743f46b9b
Dev Joe <joe.afify95@gmail.com>
2022-04-28 17:06:13 +0200

    Worked on sorted array of numbers.

--- build/index.js
+++ build/index.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("Project is ready");
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sort_1 = require("./Sort");
+const sorter = new Sort_1.Sorter([10, 3, -5, 0]);
+console.log("Unsorted: ", sorter.collection);
+sorter.sort();
+console.log("Sorted:   ", sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Project is ready");
+import { Sorter } from "./Sort";
+
+const sorter = new Sorter([10, 3, -5, 0]);
+console.log("Unsorted: ", sorter.collection);
+sorter.sort();
+console.log("Sorted:   ", sorter.collection);
